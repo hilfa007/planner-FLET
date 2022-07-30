62a52ac49305ab451ba660c3d9a051e70ed28803
hilfa vt <hilfavt007@gmail.com>
2022-07-30 20:25:19 +0530

    done

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from ctypes import alignment
-from turtle import color, onclick
+
 import flet
-from flet import Page,TextField,Checkbox,Text,FloatingActionButton,icons,Column,Row,UserControl,IconButton,colors,theme,Divider,VerticalDivider
+import datetime
+from flet import Page,TextField,Checkbox,Text,FloatingActionButton,icons,Column,Row,UserControl,IconButton,colors,theme,Divider
 
 class Task(UserControl):
-    def __init__(self, title,desc, task_delete):
+    def __init__(self,date, title,desc, task_delete):
         super().__init__()
+        self.date = date
         self.title = title
         self.desc = desc
         self.task_delete = task_delete
@@ -14,35 +15,39 @@ class Task(UserControl):
         self.task_delete(self)
 
     def build(self):
+        self.date = Text(value=self.date,expand=1,text_align="right")
         self.display_desc = TextField(value=self.desc,expand=True,visible=False,)
         self.display_task = Checkbox(value=False,label=self.title,)
         self.edit_title = TextField(expand=1,value=self.title)
         self.edit_desc = TextField(expand=1,multiline=True,min_lines=10,value=self.desc)
-        
+        self.view_title = Text(expand=1,value=self.title)
+        self.view_desc = Text(expand=1,value=self.desc)
         
 
         self.display_view = Row(
             alignment="spaceBetween",
             vertical_alignment="center",
             controls=[
+                
                 self.display_task,
                 self.display_desc,
+                self.date,
                 Row(
                     spacing=0,
                     controls=[
                         IconButton(
-                            icon=icons.VIEW_COMPACT_ROUNDED,
-                            tooltip="Show desc",
+                            icon=icons.READ_MORE,
+                            tooltip="View Diary",
                             on_click=self.show_desc,icon_color=colors.GREEN_ACCENT_400,
                         ),
                         IconButton(
                             icon=icons.CREATE_OUTLINED,
-                            tooltip="Edit To-Do",
-                            on_click=self.edit_clicked,icon_color=colors.ON_SURFACE_VARIANT,
+                            tooltip="Edit Diary",
+                            on_click=self.edit_clicked,icon_color=colors.GREEN_600,
                         ),
                         IconButton(
                             icons.DELETE_OUTLINE,
-                            tooltip="Delete To-Do",
+                            tooltip="Delete Diary",
                             on_click=self.delete_clicked,icon_color=colors.RED,
                         ),
                     ],
@@ -53,10 +58,10 @@ class Task(UserControl):
         self.item_view = Column(
                     visible=False,
                     controls=[
-                        Row(controls=[self.edit_title,]),
-                        Row(controls=[self.edit_desc,]),
+                        Row(controls=[self.view_title,]),
+                        Row(controls=[self.view_desc,]),
                         Row(controls=[IconButton(icon=icons.CLOSE,
-                        icon_color=colors.RED,tooltip="itm To-Do",on_click=self.close_clicked,)]),
+                        icon_color=colors.RED,tooltip="View Diary",on_click=self.close_clicked,)]),
                     ]
                 )
 
@@ -66,14 +71,14 @@ class Task(UserControl):
                         Row(controls=[self.edit_title,]),
                         Row(controls=[self.edit_desc,]),
                         Row(controls=[IconButton(icon=icons.DONE_OUTLINE_OUTLINED,
-                        icon_color=colors.GREEN,tooltip="Update To-Do",on_click=self.save_clicked,)]),
+                        icon_color=colors.GREEN,tooltip="Update Diary",on_click=self.save_clicked,)]),
                     ]
                 )
         return Column(controls=[self.display_view, self.edit_view,self.item_view])
 
     def show_desc(self,e):
-        self.edit_title.value  = self.display_task.label
-        self.edit_desc.value = self.display_desc.value
+        self.view_title.value  = self.display_task.label
+        self.view_desc.value = self.display_desc.value
         self.item_view.visible = True
         self.display_view.visible = True
         self.edit_view.visible = False
@@ -84,6 +89,7 @@ class Task(UserControl):
         self.edit_desc.value = self.display_desc.value
         self.display_view.visible = False
         self.edit_view.visible = True
+        self.item_view.visible = False
         self.update()
 
     def close_clicked(self,e):
@@ -103,13 +109,15 @@ class Task(UserControl):
 
 class DiaryApp(UserControl):
     def build(self):
-        self.title = TextField(hint_text="title...")
-        self.desc = TextField(hint_text="Write your diary...",multiline=True,min_lines=10)
+        self.title = TextField(hint_text="Title...",icon=icons.TITLE,)
+        self.desc = TextField(hint_text="Write your diary here...",multiline=True,border="outline", min_lines=18,max_lines=18,)
         self.tasks = Column()
-        self.theme = theme.Theme(color_scheme_seed="green")
+
 
         return [Column(
                     width=500,
+                    expand=True,
+                    scroll=True,
                     controls=[
                         self.title,
                         self.desc,
@@ -119,12 +127,12 @@ class DiaryApp(UserControl):
                 ),
                 Row(
                     controls=[
-                        Column(width=550,controls=[VerticalDivider(width=9,color=colors.AMBER_700)]),
+                        Column(width=550,),
                         Column(
                             alignment="end",
                             expand=True,
                             scroll=True,
-                            height=600,
+                            height=550,
                             controls=[
                                 self.tasks
                             ],
@@ -137,7 +145,8 @@ class DiaryApp(UserControl):
 
 
     def add_clicked(self, e):
-        task = Task(self.title.value,self.desc.value, self.task_delete)
+        self.datetime_object = datetime.date.today()
+        task = Task(self.datetime_object,self.title.value,self.desc.value, self.task_delete)
         self.tasks.controls.append(task)
         self.title.value = ""
         self.desc.value = ""
@@ -152,13 +161,18 @@ class DiaryApp(UserControl):
 def main(page):
     page.title =  "Diary"
     page.theme = theme.Theme(color_scheme_seed="green",font_family="kanit")
-    page.bgcolor = colors.AMBER_50
+    page.bgcolor = colors.LIGHT_GREEN_50
     page.horizontal_alignment="center"
-    page.padding = 50
+    page.padding = 60
+    page.scroll = True
     page.update()
 
-
+    hr = Divider(height=10,thickness=3,color=colors.GREEN_ACCENT_700,)
+    date_object = Text(datetime.date.today())
+    time_object = Text(datetime.timezone.max)
+    print(date_object)
     app = DiaryApp()
-    page.add(Divider(height=3, color=colors.AMBER_200),app)
+    page.add(Row(alignment="center",controls=[date_object,time_object]),hr,app)
+
 
 flet.app(target=main)
